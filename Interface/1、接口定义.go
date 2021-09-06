17bca6ece9621fec70f498e426113f13aeded372
lvzhenchao <1370164516@qq.com>
2021-09-06 11:38:34 +0800

    接口相关补充

--- Interface/1、接口定义.go
+++ Interface/1、接口定义.go
@@ -5,16 +5,22 @@ import "fmt"
 func main() {
 	//接口和类型的实现关系是 非嵌入式
 
-	//1、创建Mouse类型
+	//1、创建Mouse实例类型
 	m1 := Mouse{"罗技小红"}
 	fmt.Println(m1.name)
 
-	//2、创建FlashDisk
+	//2、创建FlashDisk实例类型
 	f1 := FlashDisk{"闪迪64G"}
 	fmt.Println(f1.name)
 
 	testInterface(m1)
 	testInterface(f1)
+}
+
+//测试方法
+func testInterface(usb USB)  {//传入实现接口的实例
+	usb.start()
+	usb.end()
 }
 
 type USB interface {
@@ -34,22 +40,16 @@ func (m Mouse) start()  {
 	fmt.Println(m.name,"鼠标开始工作")
 }
 
-func (f FlashDisk)start()  {
-	fmt.Println(f.name,"U盘开始工作")
-}
-
 func (m Mouse) end()  {
 	fmt.Println(m.name,"鼠标结束工作")
 }
 
-func (f FlashDisk)end()  {
-	fmt.Println(f.name,"U盘结束工作")
+func (f FlashDisk) start()  {
+	fmt.Println(f.name,"U盘开始工作")
 }
 
 
-//测试方法
-func testInterface(usb USB)  {
-	usb.start()
-	usb.end()
+func (f FlashDisk) end()  {
+	fmt.Println(f.name,"U盘结束工作")
 }
 
