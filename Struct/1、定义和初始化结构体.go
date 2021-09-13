351467620faa4c2805e594a3bcf43830aea01e69
lvzhenchao <1370164516@qq.com>
2021-09-13 09:26:39 +0800

    Update 1、定义和初始化结构体.go

--- Struct/1、定义和初始化结构体.go
+++ Struct/1、定义和初始化结构体.go
@@ -7,28 +7,28 @@ func main() {
 	//定义
 
 	//方法1
-	var p1 Person
+	var p1 Person //基本的实例化形式
 	fmt.Println(p1)
 	p1.name = "小明"
 	p1.age = 21
 	p1.sex = "男"
 	p1.address = "北京"
-	fmt.Printf("姓名：%s, 年龄：%s, 性别：%d, 地址：%s\n", p1.name,p1.age,p1.sex,p1.address)
+	fmt.Printf("姓名：%s, 年龄：%s, 性别：%d, 地址：%s\n", p1.name, p1.age, p1.sex, p1.address)
 
 	//方法2
 	p2 := Person{}
 	p2.name = "ruby"
-	p2.age  = 18
-	p2.sex  = "女"
+	p2.age = 18
+	p2.sex = "女"
 	p2.address = "南京"
-	fmt.Printf("姓名：%s, 年龄：%s, 性别：%d, 地址：%s\n", p2.name,p2.age,p2.sex,p2.address)
+	fmt.Printf("姓名：%s, 年龄：%s, 性别：%d, 地址：%s\n", p2.name, p2.age, p2.sex, p2.address)
 
 	//方法3  【常用】
 	p3 := Person{
-		name: "晓华",
-		age:20,
-		sex:"女",
-		address:"天津",
+		name:    "晓华",
+		age:     20,
+		sex:     "女",
+		address: "天津",
 	}
 	fmt.Println(p3)
 
@@ -41,14 +41,21 @@ func main() {
 	}
 	fmt.Println(p4)
 
+	//方法5
+	p5 := new(Person) //创建指针类型的机构体
+	p5.name = "lzc"
+	p5.age = 12
+	p5.sex = "男"
+	p5.address = "河北"
+	fmt.Println(p5)
 
 }
 
 //定义结构体
 type Person struct {
-	name string
-	age int
-	sex string
+	name    string
+	age     int
+	sex     string
 	address string
 }
 
