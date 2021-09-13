5f45cc3fc2edae95cba22f67e0330b902b3e8262
lvzhenchao <1370164516@qq.com>
2021-09-13 15:36:53 +0800

    Update 7、工厂方法创建结构体.go

--- Struct/7、工厂方法创建结构体.go
+++ Struct/7、工厂方法创建结构体.go
@@ -11,12 +11,15 @@ func main() {
 
 	//禁止在外部使用new函数实例化
 	wrong := new(matrix) //编译失败（matrix是私有的）
+	fmt.Println(wrong)
+
 	//强制使用工厂方法
-	right := NewMatrix(...) //实例化的唯一方式
+	right := NewMatrix(params) //实例化的唯一方式
+	fmt.Println(right)
 }
 
 type matrix struct {
-	...
+
 }
 func NewMatrix(params) *matrix {
 	m := new(matrix) // 初始化 m
